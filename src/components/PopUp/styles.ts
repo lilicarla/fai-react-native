e20f30e6b8cae683de136e446610dc7403080b80
Liliane <lilianeoliveiracarla@gmail.com>
2023-07-07 19:07:53 -0300

    fix: fix pop up left offset

--- src/components/PopUp/styles.ts
+++ src/components/PopUp/styles.ts
@@ -1,17 +1,19 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const leftOffset = (width - 300) / 2;
 
 export const Container = styled.TouchableOpacity`
   flex: 1;
   background-color:#000000aa;
-
-  
 `;
 
 export const DivPopUp = styled.TouchableOpacity`
   position: absolute;
-  width: 250px;
-  height: 300px;
-  left: 70px;
+  width: 300px;
+  height: 260px;
+  left: ${leftOffset}px;
   top: 220px;
   background-color: #FFFFFF;
   justify-content: center;
@@ -24,7 +26,7 @@ export const Title = styled.Text`
   font-weight: bold;
   text-align: center;
   font-size: 23px;
-  margin: 16px 20px 0px 20px;
+  margin: 0 20px 0 20px;
   color: #000000;
 `;
 
