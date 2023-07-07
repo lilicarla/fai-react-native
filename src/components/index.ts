33b6dbf3311784595138234fc089662440c75d0d
Liliane <lilianeoliveiracarla@gmail.com>
2023-07-07 19:09:09 -0300

    feat: export pop up component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import ThemeProvider from './ThemeProvider';
 import RoundedButton from './RoundedButton';
 import Chip from './Chips';
+import PopUp from './PopUp';
 
 export {
   ThemeProvider,
   RoundedButton,
   Chip,
+  PopUp,
 };
